0b031cd6fd15fac82d41e9f28f32ea559f7c42f2
hahaanisha <168823401+hahaanisha@users.noreply.github.com>
2025-07-18 22:00:38 +0530

    vercel issue resolve

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,6 +10,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: "/", // Change this if deploying to a subdirectory
   plugins: [
     tailwindcss(),
     react(),
@@ -40,9 +41,12 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    include: ["@clerk/clerk-react"],
+  },
   build: {
-    rollupOptions: {
-      external: ["@clerk/clerk-react"],
+    commonjsOptions: {
+      include: [/node_modules/],
     },
   },
 });
